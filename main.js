415daec89c9b688a4b9615a12c693bafd63177d8
FANR-08F <affanrabbani19@gmail.com>
2025-10-07 12:23:11 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,232 @@
+// Do your work here...
+console.log('Hello, world!');
+/**
+ * [
+ *    {
+ *      id: <int>
+ *      task: <string>
+ *      timestamp: <string>
+ *      isCompleted: <boolean>
+ *    }
+ * ]
+ */
+const books = [];
+const RENDER_EVENT = 'render-book';
+const SAVED_EVENT = 'saved-book';
+const STORAGE_KEYS = 'BOOK_APPS';
+
+function generateId() {
+  return +new Date();
+}
+
+function generateBookObject(id, book, author, year, isCompleted) {
+  return {
+    id,
+    book,
+    author,
+    year,
+    isCompleted
+  };
+}
+
+function findBook(bookId) {
+  for (const bookItem of books) {
+    if (bookItem.id === bookId) {
+      return bookItem;
+    }
+  }
+  return null;
+}
+
+function findBookIndex(bookId) {
+  for (const index in books) {
+    if (books[index].id === bookId) {
+      return index;
+    }
+  }
+  return -1;
+}
+
+
+/**
+ * Fungsi ini digunakan untuk memeriksa apakah localStorage didukung oleh browser atau tidak
+ *
+ * @returns boolean
+ */
+function isStorageExist() /* boolean */ {
+  if (typeof (Storage) === undefined) {
+    alert('Browser kamu tidak mendukung local storage');
+    return false;
+  }
+  return true;
+}
+
+/**
+ * Fungsi ini digunakan untuk menyimpan data ke localStorage
+ * berdasarkan KEY yang sudah ditetapkan sebelumnya.
+ */
+function saveData() {
+  if (isStorageExist()) {
+    const parsed /* string */ = JSON.stringify(books);
+    localStorage.setItem(STORAGE_KEYS, parsed);
+    document.dispatchEvent(new Event(SAVED_EVENT)); 
+  }
+}
+
+
+function loadDataFromStorage() {
+  const serializedData /* string */ = localStorage.getItem(STORAGE_KEYS);
+  let data = JSON.parse(serializedData);
+
+  if (data !== null) {
+    for (const book of data) {
+      books.push(books);
+    }
+  }
+
+  document.dispatchEvent(new Event(RENDER_EVENT));
+}
+
+function makeBook(bookObject) {
+
+  const {id, book, author, year, isCompleted} = bookObject;
+
+  const textTitle = document.createElement('h3');
+  textTitle.innerText = book;
+  textTitle.setAttribute('data-testid',`bookItemTitle`)
+
+  const textAuthor = document.createElement('p');
+  textAuthor.innerText = author;
+  textAuthor.setAttribute('data-testid',`bookItemAuthor`)
+
+  const years = document.createElement('p');
+  years.innerText = year;
+  years.setAttribute('data-testid',`bookItemYear`)
+
+  const textContainer = document.createElement('div');
+  textContainer.classList.add('inner');
+  textContainer.append(textTitle, textAuthor, years);
+
+  const container = document.createElement('div');
+  container.classList.add('item', 'shadow')
+  container.append(textContainer);
+  container.setAttribute('id', `book-${id}`);
+  container.setAttribute('data-testid', `bookItem`)
+
+  if (isCompleted) {
+
+    const undoButton = document.createElement('button');
+    undoButton.classList.add('undo-button');
+    undoButton.addEventListener('click', function () {
+      undoBookFromCompleted(id);
+    });
+
+    const trashButton = document.createElement('button');
+    trashButton.classList.add('trash-button');
+    trashButton.addEventListener('click', function () {
+      removeBookFromCompleted(id);
+    });
+
+    container.append(undoButton, trashButton);
+  } else {
+
+    const checkButton = document.createElement('button');
+    checkButton.classList.add('check-button');
+    checkButton.addEventListener('click', function () {
+      addBookToCompleted(id);
+    });
+
+    container.append(checkButton);
+  }
+
+  return container;
+}
+
+function addBook() {
+  const textBook = document.getElementById('bookFormTitle').value;
+  const author = document.getElementById('bookFormAuthor').value;
+  const year = document.getElementById('bookFormYear').value;
+  const isCompleted = document.getElementById('bookFormIsComplete').checked;
+
+  const generatedID = generateId();
+  const bookObject = generateBookObject(generatedID, textBook, author, year, isCompleted, false);
+  books.push(bookObject);
+
+  document.dispatchEvent(new Event(RENDER_EVENT));
+  saveData();
+}
+
+function addBookToCompleted(bookId /* HTMLELement */) {
+  const bookTarget = findBook(bookId);
+
+  if (bookTarget == null) return;
+
+  bookTarget.isCompleted = true;
+  document.dispatchEvent(new Event(RENDER_EVENT));
+  saveData();
+}
+
+function removeBookFromCompleted(bookId /* HTMLELement */) {
+  const bookTarget = findBookIndex(bookId);
+
+  if (bookTarget === -1) return;
+
+  books.splice(bookTarget, 1);
+  document.dispatchEvent(new Event(RENDER_EVENT));
+  saveData();
+}
+
+function undoBookFromCompleted(bookId /* HTMLELement */) {
+
+  const bookTarget = findBook(bookId);
+  if (bookTarget == null) return;
+
+  bookTarget.isCompleted = false;
+  document.dispatchEvent(new Event(RENDER_EVENT));
+  saveData();
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+
+  const submitForm /* HTMLFormElement */ = document.getElementById('bookForm');
+
+  submitForm.addEventListener('submit', function (event) {
+    event.preventDefault();
+    addBook();
+  });
+
+  if (isStorageExist()) {
+    loadDataFromStorage();
+  }
+});
+
+document.addEventListener(SAVED_EVENT, () => {
+  console.log('Data berhasil di simpan.');
+});
+
+document.addEventListener(RENDER_EVENT, function () {
+  const uncompletedBookList = document.getElementById('incompleteBookList');
+  const listCompleted = document.getElementById('completeBookList');
+
+  // clearing list item
+  uncompletedBookList.innerHTML = '';
+  listCompleted.innerHTML = '';
+
+  for (const bookItem of books) {
+    const bookElement = makeBook(bookItem);
+    if (bookItem.isCompleted) {
+      listCompleted.append(bookElement);
+    } else {
+      uncompletedBookList.append(bookElement);
+    }
+  }
+});
+document.addEventListener(SAVED_EVENT, function () {
+  console.log(localStorage.getItem(STORAGE_KEYS));
+  
+  const toast = document.getElementById("toast");
+  toast.classList.add("show");
+  setTimeout(() => {
+    toast.classList.remove("show");
+  }, 3000);
+});
